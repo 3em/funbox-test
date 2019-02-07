318411d49057e8e1596dc616f2c7e9e0e074e981
Alex Zemlyakov <zemka4@gmail.com>
2019-02-07 13:14:27 +0800

    fix fonts adding

--- src/assets/css/style-common.js
+++ src/assets/css/style-common.js
@@ -1,16 +1,20 @@
 import { css } from 'styled-components';
-import bgPattern from '../img/bg-pattern.svg'
-import {fontText, fontTitle, regularFontSize, regularLineHeight} from './style-variables'
+import bgPattern from '../img/bg-pattern.svg';
+import {fontText, fontTitle, regularFontSize, regularLineHeight} from './style-variables';
 
+import Exo_eot from '../fonts/Exo2.0-Thin.eot';
+import Exo_svg from "../fonts/Exo2.0-Thin.svg";
+import Exo_woff from '../fonts/Exo2.0-Thin.woff';
+import Exo_woff2 from '../fonts/Exo2.0-Thin.woff2'
 
 export const styleCommon = css`
   @font-face {
     font-family: 'Exo2.0-Thin';
-    src: url(${fontTitle}+'.eot');
-    src: url(${fontTitle}+'.eot?#iefix') format('embedded-opentype'),
-         url(${fontTitle}+'.svg#Exo2.0-Thin') format('svg'),
-         url(${fontTitle}+'.woff') format('woff'),
-         url(${fontTitle}+'.woff2') format('woff2');
+    src: url(${Exo_eot});
+    src: url(${Exo_eot}?#iefix) format('embedded-opentype'),
+         url(${Exo_svg}#Exo2.0-Thin) format('svg'),
+         url(${Exo_woff}) format('woff'),
+         url(${Exo_woff2}) format('woff2');
     font-weight: normal;
     font-style: normal;
   }
@@ -51,7 +55,7 @@ export const styleCommon = css`
     line-height: 44px;
     text-align: center;
     margin-bottom: 24px;
-    font-family: '${fontTitle}';
+    font-family: '${fontTitle}', '${fontText}';
     
     @media (max-width: 767px) {
       margin: 0 20px 24px;
